3c029c9df7bcb66cbe9232fb0ae350834e9e9ce6
Harrison Aguiar <harryaguiar@fullsail.edu>
2014-08-13 10:51:15 -0400

    Slice of Pie part 1 v1
    Set up variables

--- Expressions_worksheet/js/script.js
+++ Expressions_worksheet/js/script.js
@@ -10,13 +10,24 @@ var circ = radius * 2 * PI;
 //print out the output circumference
 console.log("The circumference of the circle is " + circ );
 
+//---------------//
+
 //Dog Years
 //Calculate how old Sparky the pit bull is in dog years based on his actual age
-var human = 5
+var human = 5 //human years variable
 
 //multiply 7 to human variable
 var dog = human * 7
 
 //print out the output dog years
 console.log("Sparky is " + human + " human years old which is " + dog + " in dog years.");
+
+//---------------//
+
+//Slice of Pie part 1
+//Calculate how much pizza each partygoer will get at the party
+var pizzza =    //pizzas ordered variable
+var people =    //partygoer variable
+var slice =     //pizza slices variable
+
 
