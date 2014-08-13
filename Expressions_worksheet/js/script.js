f713e65f131b7e5f38f245e09460bb195b5246ef
Harrison Aguiar <harryaguiar@fullsail.edu>
2014-08-13 10:14:15 -0400

    Create expression activity folder

---
+++ Expressions_worksheet/js/script.js
@@ -0,0 +1,16 @@
+
+
+var age = 38; // Number Variable
+
+var isStudent = false; //Boolean Variable
+
+var firstName = "Harrison";
+
+//Array
+var muppetNames = ["Kermit", "Miss Piggy", "Gonzo", "Rizzo"]; //0,1,2,3
+
+muppetNames[1] = "Rizzo"
+
+var num = 1
+console.log(muppetNames[num]);
+
