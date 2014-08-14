87329e1be415e7058541e8c4e938205f079d749c
Harrison Aguiar <harryaguiar@fullsail.edu>
2014-08-13 22:24:26 -0400

    Week 2 folder set up

---
+++ Expressions_Industry/js/script.js
@@ -0,0 +1,14 @@
+/*
+Harrison Aguiar
+WPF 1408 Section 01
+Expression Industry
+8-14-14
+*/
+
+
+//Problem Name
+//Calculate 
+//var
+
+//print out the output for 
+//console.log()
---
+++ Expressions_Wacky/js/script.js
@@ -0,0 +1,14 @@
+/*
+Harrison Aguiar
+WPF 1408 Section 01
+Expression Wacky
+8-14-14
+*/
+
+
+//Problem Name
+//Calculate 
+//var
+
+//print out the output for 
+//console.log()
