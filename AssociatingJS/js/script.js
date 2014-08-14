6eefc5fc4edb3e73afcf9239d31dd438fdf650d7
Harrison Aguiar <harryaguiar@fullsail.edu>
2014-08-13 22:05:11 -0400

    fixed files

--- AssociatingJS/js/script.js
+++ AssociatingJS/js/script.js
@@ -1,1 +1,18 @@
-alert("This is in the js file");
+//JavaScript
+
+var age; //declaration
+age = 23; // definition
+
+var age = 38; // Number Variable
+
+var isStudent = false; //Boolean Variable
+
+var firstName = "Harrison";
+
+//Array
+var muppetNames = ["Kermit", "Miss Piggy", "Gonzo", "Rizzo"]; //0,1,2,3
+
+muppetNames[1] = "Rizzo"
+
+var num = 1
+console.log(muppetNames[num]);
