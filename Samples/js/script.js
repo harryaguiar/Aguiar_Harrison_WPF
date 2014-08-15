13d9d4e669fcbf0745bb703d4a6b61ebbe31aec3
Harrison Aguiar <harryaguiar@fullsail.edu>
2014-08-15 18:00:10 -0400

    Array errors

--- Samples/js/script.js
+++ Samples/js/script.js
@@ -1,11 +1,19 @@
-var theArray = [];
+var givenNames = new Array();
+    var pattern = /[\w\d]{1,}/ig;
 
-for (var i = 0; i < 3; i++) {
-    var plusOne = i + 1;
-    theArray[i] = prompt("Please enter threee names: ", "Name " + plusOne);
-    document.getElementById("msg" + plusOne).innerHTML = theArray[i];
-}
 
+    for(var i=0;i<10;i++){
+var name = prompt("Enter some names. Only letters and digits are accepted!\nEntering an empty field stops asking","");
+    if(name && name.match(pattern)){givenNames.push(name);}
+    }
+
+    function displayNames(){
+        if(givenNames.length > 0){
+            document.getElementById("list").innerHTML = "<span style='color:Navy;font-    weight:bold;'>Given names are:<\/span><br><br>" + givenNames.join("<br><br>");
+        } else {
+            document.getElementById("list").innerHTML = "<span style='color:Navy;font-weight:bold;'>Nothing has been given!<\/span>";
+        }
+    }
 
 
 var experienceRate = prompt("Before I give you the total amount you made today, could you grade your work day (from 0 to 4) for us?\nNote: 0 - Really Bad to 4 - Really good")
