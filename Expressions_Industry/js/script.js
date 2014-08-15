2af7de7028b9cbb4cec2b56de3e2c250e7b616b5
Harrison Aguiar <harryaguiar@fullsail.edu>
2014-08-14 21:47:47 -0400

    Setting up prompts

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -12,6 +12,62 @@ A good example of this would be something that calculates how many pixels square
 
 
 
+/*example*/
+alert("Hello! Let's find out how many hours you worked today. Click 'Ok' to start!")
+
+//Create a js file that will tell us how old we are
+
+// Ask the user their name
+var name = prompt("Please type in your name:");
+
+//Say hello to the user and let them know what we are doing
+//ALERT the user
+
+
+//Ask the user what year ther were born in
+//Create a variable to catch their answer
+
+var timeIn = prompt("Hi " +name+ "! What time did you get in?");
+
+//Console.log out the response
+console.log(timeIn);
+
+
+//Ask the user what year ther were born in
+//Create a variable to catch their answer
+
+var timeOut = prompt("What time did you get out?");
+
+//Console.log out the response
+console.log(timeOut);
+//Current Year and subtract the year ther were born
+
+//Create a variable for the current year
+
+var dayTime = 12;
+
+//Calculate the age
+var totalTime = (12-timeIn) + (12-timeOut);
+
+//Alert user with their age
+alert(name+ " you worked " +totalTime+ " hours today.");
+
+//Let's make this more complex
+//How old will the user be in the future...
+
+//Ask the user, how many years in the future they would like to know
+var deliveries = prompt("How many deliveries did you make today?");
+console.log(deliveries);
+
+//Calculate our future age
+var futureAge = age + parseInt(yearsMore);
+console.log(futureAge);
+
+//Make the result look pretty :)
+console.log("You will be " +futureAge+ " in " + yearsMore +" years.");
+alert("You will be " +futureAge+ " in " + yearsMore +" years.");
+
+
 //Problem Name
 //Calculate 
 //var
