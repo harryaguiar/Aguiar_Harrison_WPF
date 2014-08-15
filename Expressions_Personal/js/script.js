5c0b78d7988e024d7121dac949e3ef58bf1a41c4
Harrison Aguiar <harryaguiar@fullsail.edu>
2014-08-14 23:05:02 -0400

    Trying to put ideas together

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -11,7 +11,94 @@ Code that calculates how many diapers you will need over the course of a year.
 How much paint you would need for a room in your home.
 */
 
-alert("Testing 1,2,3")
+
+var daysOfWeek = 7;
+
+/*example*/
+alert("Hi! Let's find out how much money you spent for the past week! \n Click 'Ok' to start!")
+
+
+//Create a js file that will tell us how old we are
+
+// Ask the user their name
+var name = prompt("Please type in your name:");
+console.log(name);
+
+//Say hello to the user and let them know what we are doing
+//ALERT the user
+
+
+//Ask the user what year ther were born in
+//Create a variable to catch their answer
+
+var breakfastHome = prompt("Hi " +name+ "! How many days did you have breakfast at home?");
+
+//Console.log out the response
+console.log(breakfastHome);
+
+
+//Ask the user what year ther were born in
+//Create a variable to catch their answer
+
+var breakfastSpent = prompt("How much did you spend in breakfast when you don't have it at home?");
+
+//Console.log out the response
+console.log(breakfastSpent);
+
+
+//Ask the user what year ther were born in
+//Create a variable to catch their answer
+
+var timeOut = prompt("What time are you getting out?");
+
+//Console.log out the response
+console.log(timeOut);
+//Current Year and subtract the year ther were born
+
+//Create a variable for the current year
+
+var dayTime = 12;
+
+//Calculate the age
+var totalTime = (12-timeIn) - (12-timeOut);
+
+//Alert user with their age
+alert(name+ ", you worked " +totalTime+ " hours today.");
+
+//Let's make this more complex
+//How old will the user be in the future...
+
+
+
+//Ask the user, how many years in the future they would like to know
+var deliveries = prompt("How many deliveries did you make today?");
+console.log(deliveries);
+
+var deliveryPay = 2.15;
+
+var deliveryTotal = deliveries * 2.15;
+
+//Make the result look pretty :)
+console.log("You made " +deliveries+ " deliveries today and made $" + deliveryTotal +" on deliveries.");
+alert("You made " +deliveries+ " deliveries today and made $" + deliveryTotal +" on deliveries.");
+
+var milesDriven = prompt("How many miles did you drive today?");
+console.log(milesDriven);
+
+
+var milesPay = 1.07;
+
+var milesTotal = milesDriven * 1.07;
+
+
+//Make the result look pretty :)
+console.log("You drove " +milesDriven+ " miles today and made $" + milesTotal +" on mileage.");
+alert("You drove " +milesDriven+ " miles today and made $" + milesTotal +" on mileage");
+
+var totalPay = milesTotal + deliveryTotal;
+
+console.log("Thank you for your hard work " +name+ ". You made $" +totalPay+ " today. Enjoy the rest of your day! See you tomorrow! :)");
+alert("Thank you for your hard work " +name+ ". You made $" +totalPay+ " today. Enjoy the rest of your day! See you tomorrow! :)");
 
 //Problem Name
 //Calculate 
