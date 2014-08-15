3d28dc3ccc5befaa8f2368f04d9e2f1620f0a728
Harrison Aguiar <harryaguiar@fullsail.edu>
2014-08-15 17:53:12 -0400

    Keep working on array

--- Samples/js/script.js
+++ Samples/js/script.js
@@ -1,15 +1,29 @@
+var theArray = [];
+
+for (var i = 0; i < 3; i++) {
+    var plusOne = i + 1;
+    theArray[i] = prompt("Please enter threee names: ", "Name " + plusOne);
+    document.getElementById("msg" + plusOne).innerHTML = theArray[i];
+}
+
+
+
 var experienceRate = prompt("Before I give you the total amount you made today, could you grade your work day (from 0 to 4) for us?\nNote: 0 - Really Bad to 4 - Really good")
 
-var experienceRate = ["Really Bad", "Bad", "Okay", "Good", "Really Good"]; //0,1,2,3,4
+
 
 
 
 console.log(experienceRate);
 
-experienceRate [0] = "Really Bad"
-experienceRate [2] = "Bad"
-experienceRate [3] = "Okay"
-experienceRate [4] = "Good"
-experienceRate [5] = "Really Good" //0,1,2,3,4
 
-alert(experienceRate);
+var experience = console.log(experienceRate);
+var experience = ["Really Bad", "Bad", "Okay", "Good", "Really Good"]; //0,1,2,3,4
+
+experience [0] = "Really Bad"
+experience [2] = "Bad"
+experience [3] = "Okay"
+experience [4] = "Good"
+experience [5] = "Really Good" //0,1,2,3,4
+
+alert(experience);
