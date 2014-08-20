4b80ea6a571706fd83b60aa69323feda5890bd57
Harrison Aguiar <harryaguiar@fullsail.edu>
2014-08-20 14:37:32 -0400

    Stuff your face

--- Conditionals_worksheet/js/script.js
+++ Conditionals_worksheet/js/script.js
@@ -16,4 +16,19 @@ if(temp < 75){
 }else{
 	//if it’s greater or equal to 75
 	console.log("We will go to the movies.");
-}
+};
+
+//Example
+//Stuff your face
+
+//does the competitor qualify for contest based on his weight
+var competitorWeight = 255
+
+//if the weight is equal or over 250
+if (competitorWeight >= 250) {
+	//if its over or equal 250
+	console.log("The competitor qualifies for the heavyweight division.");
+}else{
+	//it its less than 250
+	console.log("The competitor needs to gain some weight!");
+};
