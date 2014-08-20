f6b0dcc47ae9726e6fa37301690446ea9f3ea945
Harrison Aguiar <harryaguiar@fullsail.edu>
2014-08-19 22:08:13 -0400

    following screencasts - v1

--- Screencast_Conditionals/js/script.js
+++ Screencast_Conditionals/js/script.js
@@ -4,3 +4,23 @@ WPF 1408 Section 01
 Expression Personal
 8-19-14
 */
+
+if (condition) {
+	actions
+};
+
+sunny=true
+if (sunny) {
+	goToTheBeach();
+}else{
+	goToTheMovies();
+}
+
+temp = 80
+if (temp>70) {
+	goToTheBeach();
+}
+
+if (temp ==75) {
+	goToTheBeach();
+}
