a5a6b6ac2aeaa43022d3fd87876e8160fc817bcf
Harrison Aguiar <harryaguiar@fullsail.edu>
2014-08-19 22:56:30 -0400

    Conditional Logic - Else if

--- Screencast_Conditionals/js/script.js
+++ Screencast_Conditionals/js/script.js
@@ -83,7 +83,7 @@ if (kidHeight + sneakerLift > minHeight) {
 
 
 //Conditional Logic - If and Else
-
+/*
 var kidHeight = 48;
 var minHeight = 48;
 
@@ -95,8 +95,27 @@ if (kidHeight > minHeight) {
 	//code performed if condition is false
 	console.log("Sorry kid, you've got some growing to do first!");
 };
+*/
 
 
+
+//Conditional Logic - Else if
+
+var kidHeight = 52;
+var minHeight = 48;
+var wParentHeight = 45; //the height of the kid with the parent
+
+//if the kid is over 48 inches in height
+if (kidHeight > minHeight) {
+	//you can ride!
+	console.log("You can ride the coaster!");
+}if (kidHeight > wParentHeight) {
+	//you can ride with a parent present
+	console.log("You can ride, but only with a parent present.");
+}else{
+	//sorry you have growing to do
+	console.log("Sorry kid, you've got some growing to do first!");
+};
 
 
 
