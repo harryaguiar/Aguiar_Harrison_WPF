a15f703f6e80041f901a9e72fe1be83856ac7cee
Harrison Aguiar <harryaguiar@fullsail.edu>
2014-08-20 01:54:08 -0400

    Conditional Logic - Logical Operators || = Or

--- Screencast_Conditionals/js/script.js
+++ Screencast_Conditionals/js/script.js
@@ -121,7 +121,7 @@ if (kidHeight > minHeight) {
 
 
 //Conditional Logic - Logical Operators && = AND
-
+/*
 var budget = 300;
 var iPhonePrice = 199.99;
 var paycheck = 200;
@@ -132,6 +132,22 @@ if (iPhonePrice < budget && paycheck > 300) {
 }else{
 	console.log("No phone for you!!");
 };
+*/
+
+
+//Conditional Logic - Logical Operators || = Or
+
+var budget = 100;
+var iPhonePrice = 199.99;
+var wonLottery = true;
+
+//if the price of the phone is less of our budget AND if our paycheck is over 300
+if (iPhonePrice < budget || wonLottery) {
+	console.log("We can buy the phone!");
+}else{
+	console.log("No phone for you!!");
+};
+
 
 
 
