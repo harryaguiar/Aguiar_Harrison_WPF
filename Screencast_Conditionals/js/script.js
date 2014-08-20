762236d2933745f6d3ebfe5538b7e680f9dd450c
Harrison Aguiar <harryaguiar@fullsail.edu>
2014-08-20 09:46:58 -0400

    Conditional Logic - Ternary Operators Part1

--- Screencast_Conditionals/js/script.js
+++ Screencast_Conditionals/js/script.js
@@ -136,7 +136,7 @@ if (iPhonePrice < budget && paycheck > 300) {
 
 
 //Conditional Logic - Logical Operators || = Or
-
+/*
 var budget = 100;
 var iPhonePrice = 199.99;
 var wonLottery = true;
@@ -147,7 +147,22 @@ if (iPhonePrice < budget || wonLottery) {
 }else{
 	console.log("No phone for you!!");
 };
+*/
 
+
+//Conditional Logic - Ternary Operators
+
+var age = 11;
+var book;
+
+
+//if the child is under 10, they get Green Eggs and Ham, otherwise they get The Time Machine
+if (age < 10) {
+	book = "Green Eggs and Ham";
+}else{
+	book = "The Time Machine"
+};
+console.log(book);
 
 
 
