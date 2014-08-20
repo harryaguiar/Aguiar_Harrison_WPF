44a463753a3c0aeaafddf56d6a4d7ead1b4c66de
Harrison Aguiar <harryaguiar@fullsail.edu>
2014-08-19 22:26:47 -0400

    conditional Logic

--- Screencast_Conditionals/js/script.js
+++ Screencast_Conditionals/js/script.js
@@ -42,3 +42,12 @@ if (sunny) {
 
 //Conditional Logic
 
+var oldEnough = false;
+
+//if the child is old enough, print to the console "you can ride!"
+if (oldEnough) {
+	//code performed if condition is true
+	console.log("You can ride the coaster!");
+};
+console.log("What comes after");
+
