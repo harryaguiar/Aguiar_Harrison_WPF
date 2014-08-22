670dc1a502432be22495c1e64716cdd279fc9b06
Harrison Aguiar <harryaguiar@fullsail.edu>
2014-08-21 23:49:37 -0400

    coded added

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -5,10 +5,10 @@ Conditionals Industry
 8-21-14
 */
 
-//Quotation for a web design work
+//Determine if user is older enough to take driving classes, how will cost and how many weeks will take
 
 var classCost = 23.99;//cost for each class
-var classWeek = 3;//number of classes per week
+var classWeek = 4;//number of classes per week
 
 
 var name = prompt("Please type in your name"); //ask user name
@@ -17,12 +17,12 @@ if (name === "") {//validation
 }
 
 
-var age = prompt("How old are you?");
+var age = prompt("How old are you?");// ask user age
 if (age === "") {//validation
 	age = prompt("Please enter your age!");
 }
 
-licensable = (age < 18) ? "Too young":"Old enough";
+licensable = (age < 16) ? "Too young":"Old enough";// print out result if user is old enough to take driving classes
 console.log(licensable)
 
 
@@ -32,20 +32,22 @@ if (license === "") {//validation
 	license = prompt("Please enter yes or no!");
 }
 
-if (license === "no" && age > 18) {
+if (license === "no" && age > 16) {//if user doesn't have license and is over 16 
 	licenseProspect = prompt("Are you interested in taking driving classes?")
 }
 
 if (licenseProspect === "") {//validation
 	licenseProspect = prompt("Please enter yes or no!");
-}else if (licenseProspect === "yes") {
+}else if (licenseProspect === "yes") {//if user is interested on taking 
 	classesNumber = prompt("How many classes are you willing to take?")
 };
 
 var totalPrice = Number(classesNumber) + classCost
 console.log("The total for " +classesNumber+ " classes is $" +totalPrice)
+alert("The total for " +classesNumber+ " classes is $" +totalPrice)
 
 var totalWeeks = Number(classesNumber) / classWeek
 console.log("You will need " +totalWeeks+ " weeks to complete all " +classesNumber+ " classes.")
+alert("You will need " +totalWeeks+ " weeks to complete all " +classesNumber+ " classes.")
 
 
