58c46995fbb09d9228ff23df20bb54cdec0dcd89
Harrison Aguiar <harryaguiar@fullsail.edu>
2014-08-21 20:57:30 -0400

    checking operations v1

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -24,21 +24,42 @@ if (team === "") {
 	team = prompt("Please enter your team name!");
 };
 
-var gameResult = prompt("Did your team win, draw or lost the last game?");
+var gameResult = prompt("Did your team had a win, drawn or a lost on the last game?");
 
 if (gameResult === "") {
-	gameResult = prompt("Please enter your team name!");
+	gameResult = prompt("Please answer if your team had a win, drawn or a lost!");
 };
 
 if (gameResult === "win"){
-	console.log("Your team " +team+ " won " +victory+ "points from the last game.");
-}else if (gameResult === "draw") {
-	console.log("Your team " +team+ " won " +drawn+ "points from the last game.");
+	console.log("Your team " +team+ " won " +victory+ " points from the last game.");
+	alert("Your team " +team+ " won " +victory+ " points from the last game.");
+}else if (gameResult === "drawn") {
+	console.log("Your team " +team+ " won " +drawn+ " points from the last game.");
+	alert("Your team " +team+ " won " +drawn+ " points from the last game.");
 }else if (gameResult === "lost") {
-	console.log("Your team " +team+ " won " +lost+ "points from the last game.");
-}else{
+	console.log("Your team " +team+ " won " +lost+ " points from the last game.");
+	alert("Your team " +team+ " won " +lost+ " points from the last game.");
+};
 
-}
+var pointsBefore = prompt("How many points did your team have before this game?")
+
+if (pointsBefore === "") {
+	pointsBefore = prompt("Please answer how many points your team had before this game!");
+};
+
+if (gameResult === "win"){
+	var totalPoints = "victory" + pointsBefore
+	console.log(+team+ " has now " +totalPoints+ " on the league.");
+	alert(+team+ " has now " +totalPoints+ " on the league.");
+}else if (gameResult === "drawn") {
+	var totalPoints = "drawn" + pointsBefore
+	console.log(+team+ " has now " +totalPoints+ " on the league.");
+	alert(+team+ " has now " +totalPoints+ " on the league.");
+}else if (gameResult === "lost") {
+	var totalPoints = drawn + pointsBefore
+	console.log(+team+ " has now " +totalPoints+ " on the league.");
+	alert(+team+ " has now " +totalPoints+ " on the league.");
+};
 
 
 //result
