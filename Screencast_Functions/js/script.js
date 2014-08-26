0b1ac18c52e90dbdae773dcf300c64b3c38e2e8b
Harrison Aguiar <harryaguiar@fullsail.edu>
2014-08-25 22:02:11 -0400

    screencast v1

---
+++ Screencast_Functions/js/script.js
@@ -0,0 +1,52 @@
+/*
+Harrison Aguiar
+WPF 1408 Section 01
+Screencast: Functions
+8-25-14
+*/
+
+function functionName(){
+	//conde the function runs
+}
+
+
+function outptMsg(){
+	console.log("Hello World");
+}
+
+function calcArea(){
+	var width = 20;
+	var height = 30;
+	var area = width * height;
+	console.log(area);
+}
+
+calcArea();
+calcArea();
+calcArea();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
