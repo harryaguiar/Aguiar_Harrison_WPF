af27572f6608082bd4c4d72f6bc7cd6e1b6ffd72
Harrison Aguiar <harryaguiar@fullsail.edu>
2014-08-25 22:46:20 -0400

    Screencast Functions - Done

--- Screencast_Functions/js/script.js
+++ Screencast_Functions/js/script.js
@@ -88,8 +88,17 @@ function calcAreaP(width, height){
 	console.log(area); //no return
 }
 
+/*----------------Anonymous Functions--------------------------*/
 
+var calcArea = function (width, height){//defining
+//code the function runs
+	var area = width * height;
+	return area;
+}
 
+var a = calcArea(20, 30); //invoking
+
+console.log(a);
 
 
 
