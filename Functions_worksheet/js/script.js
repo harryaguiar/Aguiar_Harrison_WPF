0053b227845977638296b1565d943738b468e300
Harrison Aguiar <harryaguiar@fullsail.edu>
2014-08-26 22:17:22 -0400

    Added first problem to file

--- Functions_worksheet/js/script.js
+++ Functions_worksheet/js/script.js
@@ -17,3 +17,17 @@ function calculateArea(w, h )
 {
      return w *h;
 }
+
+/*-------------------------------------------------------*/
+
+//Circumference
+
+//Calculate the circumference of a circle.
+
+/*
+Parameter(s) for function:
+Radius of the circle
+Return:
+Circumference of the circle
+Result to print to the console:
+“The circumference of the circle is X”;*/
