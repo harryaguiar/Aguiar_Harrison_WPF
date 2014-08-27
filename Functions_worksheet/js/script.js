c5c29a67a0fca28382d8c055a7dcd4e317910c72
Harrison Aguiar <harryaguiar@fullsail.edu>
2014-08-26 22:18:27 -0400

    Added second problem to file

--- Functions_worksheet/js/script.js
+++ Functions_worksheet/js/script.js
@@ -31,3 +31,21 @@ Return:
 Circumference of the circle
 Result to print to the console:
 “The circumference of the circle is X”;*/
+
+
+/*-------------------------------------------------------*/
+
+//Stung!
+
+/*
+It takes 8.666666667 bee stings per pound to kill an animal. 
+//Calculate how many bee stings are needed to kill an animal in a function
+
+Given:
+Victim’s weight (in pounds)     
+Parameter(s) for function:
+Victim’s weight (in pounds)
+Return:
+Number of Bee stings
+Result to print to the console:
+“It takes X bee stings to kill this animal.*/
