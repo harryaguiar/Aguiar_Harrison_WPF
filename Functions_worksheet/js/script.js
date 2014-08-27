0e1dfb00efee1d27782f987e241bf50a193c3e23
Harrison Aguiar <harryaguiar@fullsail.edu>
2014-08-26 22:14:54 -0400

    Example problem added

--- Functions_worksheet/js/script.js
+++ Functions_worksheet/js/script.js
@@ -4,3 +4,16 @@ WPF 1408 Section 01
 Function Workshhet
 8-26-14
 */
+
+//Problem: Calculate the area of a rectangle given the width and height of the rectangle.
+
+//Calculate the Area of a Rectangle
+var width = 5;
+var height = 6;
+var area = calculateArea(width, height);
+console.log(“The Area of the Rectangle is “ + area);
+
+function calculateArea(w, h )
+{
+     return w *h;
+}
