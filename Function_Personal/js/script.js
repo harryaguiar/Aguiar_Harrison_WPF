cf34af084dec30f6b87ab0284280320a5ab40f68
Harrison Aguiar <harryaguiar@fullsail.edu>
2014-08-28 19:05:46 -0400

    Folder created

---
+++ Function_Personal/js/script.js
@@ -0,0 +1,72 @@
+/*
+Harrison Aguiar
+WPF 1408 Section 01
+Conditionals Personal
+8-21-14
+*/
+
+//Annual spending
+
+//Determine how much user spend in a year with monthly bills
+
+//Given
+var yearMonths = 12;//number of months in a year
+var answerNo = 0;//value given for every "no" as answer
+
+var name = prompt("Please type in your name")//ask user's name
+console.log(name)// print out result
+if (name === "") {//validation
+	name = prompt("Please enter your name!");
+};
+
+var rent = prompt("Do you pay rent?")//ask user if pays for rent
+console.log(rent)// print out result
+if (rent === "") {//validation
+	rent = prompt("Please enter yes or no!");
+}else if (rent === "yes") {//if answer is yes
+	var rentBill = prompt("How much do you pay?")//ask user how much pays for rent every month
+	console.log("You pay $" +rentBill+ " on rent.")// print out result
+}else if (rent === "no") {//if answer is no
+	var rentBill = answerNo;
+	console.log("You pay $" +rentBill+ " for your cellphone.")// print out result
+};
+
+var cellphone = prompt("Do you pay for your cellphone bill?")//ask user if pays for cellphone
+console.log(cellphone)// print out result
+if (cellphone === "") {//validation
+	cellphone = prompt("Please enter yes or no!");
+}else if (cellphone === "yes") {//if answer is yes
+	var cellphoneBill = prompt("How much do you pay?")//ask user how much pays for cellphone bill every month
+	console.log("You pay $" +cellphoneBill+ " for your cellphone.")// print out result
+}else if (cellphone === "no") {//if answer is no
+	var cellphoneBill = answerNo;
+	console.log("You pay $" +cellphoneBill+ " for your cellphone.")// print out result
+};
+
+var cable = prompt("Do you pay for your cable?")//ask user if pays for cable
+console.log(cable)// print out result
+if (cable === "") {//validation
+	cable = prompt("Please enter yes or no!");
+}else if (cable === "yes") {//if answer is yes
+	var cableBill = prompt("How much do you pay?")//ask user how much pays for cable every month
+	console.log("You pay $" +cableBill+ " for your cable.")// print out result
+}else if (cable === "no") {//if answer is no
+	var cableBill = answerNo;
+	console.log("You pay $" +cableBill+ " for your cable.")// print out result
+};
+
+var train = prompt("Do you pay for the monthly metro card?")//ask user if pays for metro card every month
+console.log(train)// print out result
+if (train === "") {//validation
+	train = prompt("Please enter yes or no!");
+}else if (train === "yes") {//if answer is yes
+	var trainBill = prompt("How much do you pay?")//ask user how much pays for metro card every month
+	console.log("You pay $" +trainBill+ " for your monthly metro card.")// print out result
+}else if (train === "no") {//if answer is no
+	var trainBill = answerNo;
+	console.log("You pay $" +trainBill+ " for your monthly metro card.")// print out result
+};
+
+var monthlyBill = (Number(rentBill) + Number(cellphoneBill) + Number(cableBill) + Number(trainBill)) * yearMonths//calculation for the annual spending on bills
+console.log("You spend $" +monthlyBill+ " on your monthly bills for the year.")// print out result
+alert("You spend $" +monthlyBill+ " on your monthly bills for the year.")// alert user the result
---
+++ Function_Wacky/js/script.js
@@ -0,0 +1,64 @@
+/*
+Harrison Aguiar
+WPF 1408 Section 01
+Conditionals Wacky
+8-21-14
+*/
+
+// Soccer table
+
+//Determine how many points will a soccer team have after a game result 
+
+
+//Given
+var victory = 3; //points given for a victory
+var tie = 1; // points given for a tie
+var lost = 0; // points given for a lost
+
+
+//ask user what team does he chear for
+var team = prompt("What is your team?");
+
+if (team === "") {//validation
+	team = prompt("Please enter your team name!");
+};
+
+//ask user if team won, tied or lost on the last game
+var gameResult = prompt("Did your team had a win, tie or a lost on the last game?");
+	console.log(gameResult)// print out result
+
+if (gameResult === "") {//validation
+	gameResult = prompt("Please answer if your team had a win, tie or a lost!");
+};
+
+if (gameResult === "win"){//if the answer is a win
+	console.log("Your team " +team+ " won " +victory+ " points from the last game.");// print out result
+	alert("Your team " +team+ " won " +victory+ " points from the last game.");// alert user the result
+}else if (gameResult === "tie") {//if the answer is a tie
+	console.log("Your team " +team+ " won " +tie+ " points from the last game.");// print out result
+	alert("Your team " +team+ " won " +tie+ " points from the last game.");// alert user the result
+}else if (gameResult === "lost") {//if the answer is a lost
+	console.log("Your team " +team+ " won " +lost+ " points from the last game.");// print out result
+	alert("Your team " +team+ " won " +lost+ " points from the last game.");// alert user the result
+};
+
+var pointsBefore = prompt("How many points did your team have before this game?")
+	console.log(pointsBefore)// print out result
+
+if (pointsBefore === "") {//validation
+	pointsBefore = prompt("Please answer how many points your team had before this game!");
+};
+
+if (gameResult === "win"){//if team won add with points team had before the game
+	var totalPoints = parseInt(victory) + parseInt(pointsBefore)//points given + points had before
+	console.log(team+ " has now " +totalPoints+ " on the league.");// print out result
+	alert(team+ " has now " +totalPoints+ " on the league.");// alert user the result
+}else if (gameResult === "tie") {//if team tied add with points team had before the game
+	var totalPoints = parseInt(tie) + parseInt(pointsBefore)//points given + points had before
+	console.log(team+ " has now " +totalPoints+ " on the league.");// print out result
+	alert(team+ " has now " +totalPoints+ " on the league.");// alert user the result
+}else if (gameResult === "lost") {//if team lost add with points team had before the game
+	var totalPoints = parseInt(lost) + parseInt(pointsBefore)//points given + points had before
+	console.log(team+ " has now " +totalPoints+ " on the league.");// print out result
+	alert(team+ " has now " +totalPoints+ " on the league.");// alert user the result
+};
