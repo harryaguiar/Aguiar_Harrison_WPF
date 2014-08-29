25fcdc3e9c62b24c245982b6330de95f83e47577
Harrison Aguiar <harryaguiar@fullsail.edu>
2014-08-28 22:07:42 -0400

    Personal Problem solved

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -5,45 +5,22 @@ Functions Industry
 8-27-14
 */
 
-//Determine if user is older enough to take driving classes, how will cost and how many weeks will take
 
-var classCost = 23.99;//cost for each class
-var classWeek = 4;//number of classes per week
+var name = prompt("Please type in your name.")
 
-var name = prompt("Please type in your name"); //ask user name
-if (name === "") {//validation
-	name = prompt("Please enter your name!");
-};
-
-var age = prompt("How old are you?");// ask user age
-if (age === "") {//validation
-	age = prompt("Please enter your age!");
-};
-
-licensable = (age < 16) ? "Too young":"Old enough"//if user is old enough to take driving classes
-console.log(licensable)//print out result
-
-var license = prompt("Do you have a driver's license?");// ask user if he has driver's license
-
-if (license === "") {//validation
-	license = prompt("Please enter yes or no!");
+while(name===""){
+	name=prompt("Don't leave this blank, it's required!\nPlease type in your name!")
 }
 
-if (license === "no" && age > 16) {//if user doesn't have license and is over 16 
-	licenseProspect = prompt("Are you interested in taking driving classes?");// ask user if he is interested in taking driving classes
-};
+console.log(name);
 
-if (licenseProspect === "") {//validation
-	licenseProspect = prompt("Please enter yes or no!");
-}else if (licenseProspect === "yes") {//if user is interested on taking
-	classesNumber = prompt("How many classes are you willing to take?"); //if so, ask user how many classes he would take
-};
 
-var totalPrice = Number(classesNumber) * classCost //calculate cost for all classes
-console.log("The total for " +classesNumber+ " classes is $" +totalPrice) //print out result
-alert("The total for " +classesNumber+ " classes is $" +totalPrice) //alert result to user
 
-var totalWeeks = Number(classesNumber) / classWeek //calculate how many weeks will take all classes
-console.log("You will need " +totalWeeks+ " weeks to complete all " +classesNumber+ " classes.") // print out result
-alert("You will need " +totalWeeks+ " weeks to complete all " +classesNumber+ " classes.") // alert result to user 
 
+var total = calcArea(30, 20);
+
+function calcArea(w, h){
+	var area = w * h;
+	return area; //function spitting the info out
+}
+console.log(total);
--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -5,68 +5,62 @@ Functions Personal
 8-27-14
 */
 
-//Annual spending
 
-//Determine how much user spend in a year with monthly bills
+//Voting poll
 
-//Given
-var yearMonths = 12;//number of months in a year
-var answerNo = 0;//value given for every "no" as answer
 
-var name = prompt("Please type in your name")//ask user's name
-console.log(name)// print out result
-if (name === "") {//validation
-	name = prompt("Please enter your name!");
-};
+var name = prompt("Please type in your name.")
 
-var rent = prompt("Do you pay rent?")//ask user if pays for rent
-console.log(rent)// print out result
-if (rent === "") {//validation
-	rent = prompt("Please enter yes or no!");
-}else if (rent === "yes") {//if answer is yes
-	var rentBill = prompt("How much do you pay?")//ask user how much pays for rent every month
-	console.log("You pay $" +rentBill+ " on rent.")// print out result
-}else if (rent === "no") {//if answer is no
-	var rentBill = answerNo;
-	console.log("You pay $" +rentBill+ " for your cellphone.")// print out result
-};
+while(name===""){
+	name=prompt("Don't leave this blank, it's required!\nPlease type in your name!")
+}
 
-var cellphone = prompt("Do you pay for your cellphone bill?")//ask user if pays for cellphone
-console.log(cellphone)// print out result
-if (cellphone === "") {//validation
-	cellphone = prompt("Please enter yes or no!");
-}else if (cellphone === "yes") {//if answer is yes
-	var cellphoneBill = prompt("How much do you pay?")//ask user how much pays for cellphone bill every month
-	console.log("You pay $" +cellphoneBill+ " for your cellphone.")// print out result
-}else if (cellphone === "no") {//if answer is no
-	var cellphoneBill = answerNo;
-	console.log("You pay $" +cellphoneBill+ " for your cellphone.")// print out result
-};
+console.log(name);
 
-var cable = prompt("Do you pay for your cable?")//ask user if pays for cable
-console.log(cable)// print out result
-if (cable === "") {//validation
-	cable = prompt("Please enter yes or no!");
-}else if (cable === "yes") {//if answer is yes
-	var cableBill = prompt("How much do you pay?")//ask user how much pays for cable every month
-	console.log("You pay $" +cableBill+ " for your cable.")// print out result
-}else if (cable === "no") {//if answer is no
-	var cableBill = answerNo;
-	console.log("You pay $" +cableBill+ " for your cable.")// print out result
-};
+/*-------------------------------------------------------*/
 
-var train = prompt("Do you pay for the monthly metro card?")//ask user if pays for metro card every month
-console.log(train)// print out result
-if (train === "") {//validation
-	train = prompt("Please enter yes or no!");
-}else if (train === "yes") {//if answer is yes
-	var trainBill = prompt("How much do you pay?")//ask user how much pays for metro card every month
-	console.log("You pay $" +trainBill+ " for your monthly metro card.")// print out result
-}else if (train === "no") {//if answer is no
-	var trainBill = answerNo;
-	console.log("You pay $" +trainBill+ " for your monthly metro card.")// print out result
-};
+var speedingTicket = prompt("Did you get a speeding ticket?")
 
-var monthlyBill = (Number(rentBill) + Number(cellphoneBill) + Number(cableBill) + Number(trainBill)) * yearMonths//calculation for the annual spending on bills
-console.log("You spend $" +monthlyBill+ " on your monthly bills for the year.")// print out result
-alert("You spend $" +monthlyBill+ " on your monthly bills for the year.")// alert user the result
+if (speedingTicket === "") {//validation
+	speedingTicket = prompt("Please enter yes or no!");
+}else if (speedingTicket === "yes") {//if user is interested on taking
+	speedLimit = prompt("What was the speed limit posted?"); //if so, ask user how many classes he would take
+}else{
+	wrongPlace = alert("Thank you for your visit! We are here to help people who have got a speeding ticket!")
+}
+
+
+console.log(speedLimit);
+
+
+/*-------------------------------------------------------*/
+
+var speedDriven = prompt("How fast where you going?")
+console.log(speedDriven);
+
+while(speedDriven===""){
+	speedDriven = prompt("Please type in how fast you were driving!")
+}
+
+
+console.log(speedDriven);
+
+/*-------------------------------------------------------*/
+
+var mileageCost = prompt("What is the cost for every mile you go over the limit in your county?")
+while(mileageCost===""){
+	mileageCost = prompt("Please type in how fast you were driving!")
+}
+
+console.log(mileageCost);
+/*-------------------------------------------------------*/
+
+var ticketPrice = calcTicket(speedLimit, speedDriven, mileageCost);
+
+function calcTicket(L, D, C){
+	var ticket = (D - L) * C;
+	return ticket; //function spitting the info out
+}
+console.log(ticketPrice);
+
+
